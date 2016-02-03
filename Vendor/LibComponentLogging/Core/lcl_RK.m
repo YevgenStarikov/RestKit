db8ab03799e586a77f6ee17584ce71dc47c64651
razmara <majid.razmara@gmail.com>
2016-02-02 22:06:36 -0800

    testing a path problem

--- Vendor/LibComponentLogging/Core/lcl_RK.m
+++ Vendor/LibComponentLogging/Core/lcl_RK.m
@@ -23,7 +23,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import <RestKit/Support/lcl_RK.h>
+#import <RestKitTV/Support/lcl_RK.h>
 #include <string.h>
 
 
